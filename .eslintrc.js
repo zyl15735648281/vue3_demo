84191f2b2a622a0c43a563ea54bc40069cf0ffb3
zhangyanli <zhangyanli@zclhgroup.com>
2022-05-31 21:14:47 +0800

    fix: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,71 @@ module.exports = {
     'vue',
   ],
   rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'off', // 允许局部变量替换全局变量
+    // 允许在 case 或 default 子句中出现词法声明
+    'no-case-declarations': 'off',
+    // 未使用的class方法可以存在
+    'class-methods-use-this': 'off',
+    // 可以使用a&&b()
+    'no-unused-expressions': 'off',
+    // 函数入参不能直接修改当前参数（部分必须要的设置排除）
+    'no-param-reassign': 'off',
+    'vue/script-setup-uses-vars': 'error',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/valid-define-props': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'space-before-function-paren': 'off',
+    'vue/attributes-order': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'prettier/prettier': 'off',
+    'vue/no-dupe-keys': [
+      'error',
+      {
+        groups: [],
+      },
+    ],
   },
 };
